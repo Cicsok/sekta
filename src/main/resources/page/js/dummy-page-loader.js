1d6ff97e497c646ff79d1f2452c8cacefe750177
SzakacsAgi <szakacs.agi21@gmail.com>
2022-07-10 08:45:42 +0200

    URL added to the class

--- src/main/resources/page/js/dummy-page-loader.js
+++ src/main/resources/page/js/dummy-page-loader.js
@@ -2,7 +2,7 @@ class DummyPageLoader {
     constructor() {}
 
     static async load() {
-        let dateOrTime = await DummyRESTAPICaller.getData('http://localhost:8080/sekta/api/dummy');
+        let dateOrTime = await DummyRESTAPICaller.getData();
         DummyPageContentPopulator.displayDateTime('time', dateOrTime.time);
         DummyPageContentPopulator.displayDateTime('date', dateOrTime.date);
     }
