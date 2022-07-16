060a294095a95fb77459cf99f0320b6ba5053c3f
gabichelsea <gabilampard@gmail.com>
2022-07-16 23:42:16 +0200

    Handle recursive solution

--- src/main/resources/page/js/source-loader/internalJSLoader.js
+++ src/main/resources/page/js/source-loader/internalJSLoader.js
@@ -7,28 +7,24 @@ class InternalJSLoader {
     load(scriptURLs) {
         console.log(scriptURLs);
         const startIndex = 0;
-        this.loadSingleItem(startIndex, scriptURLs);
+        InternalJSLoader.loadSingleItem(startIndex, scriptURLs);
     }
 
-    loadSingleItem(index, scriptURLs) {
+    static loadSingleItem(index, scriptURLs) {
         console.log(index);
         if (index >= scriptURLs.length) {
             return false;
         }
 
         var script = document.createElement('script');
+
         script.onload = function() {
             console.log("Script loaded: ", scriptURLs[index]);
-            this.caller.callRecursiveFunction(index+1, scriptURLs);
+            InternalJSLoader.loadSingleItem(index+1, scriptURLs);
         }
+
         script.src = scriptURLs[index];
 
         document.head.appendChild(script);
-    }
-
-    caller = function RecursiveFunctionCaller() {
-        function callRecursiveFunction(index, scriptURLs) {
-            this.loadSingleItem(index, scriptURLs);
-        }
     }
 }
